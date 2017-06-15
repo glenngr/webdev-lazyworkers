8b9869a40c43fabcaec83a44ac33b16f42fbcd16
Glenn Greibesland <glenn@glenng.no>
2017-06-15 20:19:41 +0200

    add fibonacci-serbvice to noworker module

--- src/app/fibonacci-noworker/fibonacci-noworker.module.ts
+++ src/app/fibonacci-noworker/fibonacci-noworker.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FibonacciNoworkerComponent } from './fibonacci-noworker.component';
 import { FibonacciNoworkerRoutingModule } from './fibonacci-noworker-routing.module';
+import { FibonacciService } from './fibonacci.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FibonacciNoworkerRoutingModule
   ],
-  declarations: [FibonacciNoworkerComponent]
+  declarations: [FibonacciNoworkerComponent],
+  providers: [FibonacciService]
 })
 export class FibonacciNoworkerModule { }
---
+++ src/app/fibonacci-noworker/fibonacci.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class FibonacciService {
+
+  constructor() { }
+  public generate(n: number): number[] {
+    return this.generateFibonacciSeries(n);
+  }
+
+  private calculateNextFibonacciValue(n: number): number {
+    let s = 0;
+
+    if (n === 0) {
+      return (s);
+    }
+    if (n === 1) {
+      s += 1;
+      return (s);
+    } else {
+      return (this.calculateNextFibonacciValue(n - 1) + this.calculateNextFibonacciValue(n - 2));
+    }
+  }
+
+  private generateFibonacciSeries(n): number[] {
+    const results: number[] = [];
+    for (let i = 0; i <= n - 1; i++) {
+      results.push(this.calculateNextFibonacciValue(i));
+    }
+    console.log(results);
+    return results;
+  }
+}
