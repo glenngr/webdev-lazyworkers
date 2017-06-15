cd5ed34d62540679dd6e452a90e8b6f2c653be22
Glenn Greibesland <glenn@glenng.no>
2017-06-15 19:29:21 +0200

    show nav buttons inside a md-toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
