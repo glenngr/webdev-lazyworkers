d57a3fe6c72ad04f36a981379fdcb7b8fdcc7b52
Glenn Greibesland <glenn@glenng.no>
2017-06-15 18:45:51 +0200

    add lazy-loading routing for fibonacci modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    children: []
-  }
+    children: [],
+  },
+  {
+    path: 'fibonacci-noworker',
+    loadChildren: './fibonacci-noworker/fibonacci-noworker.module#FibonacciNoworkerModule'
+  },
+  {
+    path: 'fibonacci-worker',
+    loadChildren: './fibonacci-worker/fibonacci-worker.module#FibonacciWorkerModule'
+  },
+  { path: '**', pathMatch: 'full', redirectTo: '/' }
 ];
 
 @NgModule({
