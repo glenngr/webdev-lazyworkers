df0256db999701e3c672559060688e065a387c2c
Glenn Greibesland <glenn@glenng.no>
2017-06-15 21:04:39 +0200

    FibonacciWorkerService implements FibonacciServiceInterface

--- src/app/fibonacci-worker/fibonacci-worker.service.ts
+++ src/app/fibonacci-worker/fibonacci-worker.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
-export class FibonacciWorkerService {
+import { FibonacciServiceInterface } from '../shared/fibonacci.service';
 
+@Injectable()
+export class FibonacciWorkerService implements FibonacciServiceInterface {
   constructor() { }
 
+  generate(n: number): number[] {
+    throw new Error("Method not implemented.");
+  }
 }
