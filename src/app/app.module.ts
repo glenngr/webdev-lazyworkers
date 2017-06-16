ba452d1a51af8551d60830f1b3a697d905f4dd54
Glenn Greibesland <glenn@glenng.no>
2017-06-16 20:28:06 +0200

    add commented out feature modules to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MdSidenavModule, MdListModule} from '@angular/material';
 import { SharedModule } from './shared/shared.module';
+// import { FibonacciNoworkerModule } from './fibonacci-noworker/fibonacci-noworker.module';
+// import { FibonacciWorkerModule } from './fibonacci-worker/fibonacci-worker.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
