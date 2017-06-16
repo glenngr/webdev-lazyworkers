35573b71159fb47b7078ec06c62596afb4906814
Glenn Greibesland <glenn@glenng.no>
2017-06-16 18:58:32 +0200

    add fibonacci worker javascript file

---
+++ src/app/fibonacci-worker/fibworker.js
@@ -0,0 +1,33 @@
+addEventListener('message', function (e) {
+    console.log('incoming msg', e);
+    var data = e.data;
+    switch (data.cmd) {
+        case 'generate':
+            postMessage(generateFibonacciSeries(data.msg));
+    }
+}, false);
+function generate(n) {
+    return this.generateFibonacciSeries(n);
+}
+function calculateNextFibonacciValue(n) {
+    var s = 0;
+    var returnValue;
+    if (n == 0) {
+        return (s);
+    }
+    if (n == 1) {
+        s += 1;
+        return (s);
+    }
+    else {
+        return (this.calculateNextFibonacciValue(n - 1) + this.calculateNextFibonacciValue(n - 2));
+    }
+}
+function generateFibonacciSeries(n) {
+    var results = [];
+    for (var i = 0; i <= n - 1; i++) {
+        results.push(this.calculateNextFibonacciValue(i));
+    }
+    console.log(results);
+    return results;
+}
