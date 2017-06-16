5b0b3a30302d2a0ea859ba48865a26fffad240e1
Glenn Greibesland <glenn@glenng.no>
2017-06-16 20:08:08 +0200

    remove CommonModule import from feature modules

--- src/app/fibonacci-noworker/fibonacci-noworker.module.ts
+++ src/app/fibonacci-noworker/fibonacci-noworker.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { FibonacciNoworkerComponent } from './fibonacci-noworker.component';
 import { FibonacciNoworkerRoutingModule } from './fibonacci-noworker-routing.module';
--- src/app/fibonacci-worker/fibonacci-worker.module.ts
+++ src/app/fibonacci-worker/fibonacci-worker.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FibonacciWorkerComponent } from './fibonacci-worker.component';
 import { FibonacciNoworkerRoutingModule } from './fibonacci-worker-routing.module';
 import { FibonacciWorkerService } from './fibonacci-worker.service';
