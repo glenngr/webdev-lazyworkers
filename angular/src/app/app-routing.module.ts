cf732a71465dd07e1cd6f37651f8767c78e8e2e2
Glenn Greibesland <glenn@glenng.no>
2017-06-18 19:39:15 +0200

    revert to no lazy-loading

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FibonacciNoworkerComponent } from './fibonacci-noworker/fibonacci-noworker.component';
+import { FibonacciWorkerComponent } from './fibonacci-worker/fibonacci-worker.component';
 
 const routes: Routes = [
   {
@@ -7,18 +10,21 @@ const routes: Routes = [
     pathMatch: 'full',
     redirectTo: 'home'
   },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomeModule'
-  },
-  {
-    path: 'fibonacci-noworker',
-    loadChildren: './fibonacci-noworker/fibonacci-noworker.module#FibonacciNoworkerModule'
-  },
-  {
-    path: 'fibonacci-worker',
-    loadChildren: './fibonacci-worker/fibonacci-worker.module#FibonacciWorkerModule'
-  },
+  // {
+  //   path: 'home',
+  //   loadChildren: './home/home.module#HomeModule'
+  // },
+  // {
+  //   path: 'fibonacci-noworker',
+  //   loadChildren: './fibonacci-noworker/fibonacci-noworker.module#FibonacciNoworkerModule'
+  // },
+  // {
+  //   path: 'fibonacci-worker',
+  //   loadChildren: './fibonacci-worker/fibonacci-worker.module#FibonacciWorkerModule'
+  // },
+  { path: 'home', component: HomeComponent },
+  { path: 'fibonacci-noworker', component: FibonacciNoworkerComponent },
+  { path: 'fibonacci-worker', component: FibonacciWorkerComponent },
   { path: '**', pathMatch: 'full', redirectTo: '/' }
 ];
 
